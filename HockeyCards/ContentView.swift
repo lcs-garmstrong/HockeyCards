d021317a1ae7696a9f6a1a48932ae8d7c62f0e52
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-12 15:39:35 -0500

    Started Auston Matthews hockey card. Completed Z stack with blue/white frame, image and added leafs logo.

--- HockeyCards/ContentView.swift
+++ HockeyCards/ContentView.swift
@@ -10,12 +10,56 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                
+                Color("Dark Blue")
+                
+                Color.white
+                    .padding(20)
+                
+                Image("Auston Matthews")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(30)
+                
+                HStack {
+                    VStack {
+                        Text("#34")
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Image("LeafsLogo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150)
+                    }
+                }
+            }
+            
+            Text("Auston Mattews")
+            Text("Centerman / Toronto Maple Leafs")
+            
+            HStack{
+                Text("GP")
+                Text("G")
+                Text("A")
+                Text("PTS")
+                Text("+/-")
+                Text("PIM")
+            }
+            .padding()
+            
+            Text("Player Description")
+            Text("Auston Taylour Matthews (born September 17, 1997) is an American professional ice hockey center and alternate captain for the Toronto Maple Leafs of the National Hockey League (NHL).")
+            
         }
-        .padding()
     }
 }
 
