1166c2f8cc8d11833a0314a47b94e28f40f403f4
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-12 14:34:11 -0500

    Initial Commit

---
+++ HockeyCards/HockeyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyCardsApp.swift
+//  HockeyCards
+//
+//  Created by Graeme Armstrong on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct HockeyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
