6bdfdf5210d68778b8100046eada51701df5210a
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-12 18:07:20 -0500

    Made some font and other padding changes to card

--- HockeyCards/ContentView.swift
+++ HockeyCards/ContentView.swift
@@ -13,19 +13,23 @@ struct ContentView: View {
             ZStack {
                 
                 Color("Dark Blue")
+                    .ignoresSafeArea()
                 
                 Color.white
-                    .padding(20)
+                    .padding(15)
+                    .ignoresSafeArea()
                 
                 Image("Auston Matthews")
                     .resizable()
                     .scaledToFit()
-                    .padding(30)
+                    .padding(40)
                 
                 HStack {
                     VStack {
                         Text("#34")
-                            .foregroundColor(.white)
+                            .foregroundColor(Color("Dark Blue"))
+                            .font(Font.custom("San Francisco", size: 35))
+                            .padding(20)
                         Spacer()
                     }
                     Spacer()
@@ -43,23 +47,41 @@ struct ContentView: View {
                 }
             }
             
-            Text("Auston Mattews")
-            Text("Centerman / Toronto Maple Leafs")
-            
-            HStack{
-                Text("GP")
-                Text("G")
-                Text("A")
-                Text("PTS")
-                Text("+/-")
-                Text("PIM")
+            HStack {
+                Image("NHL logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40)
+                
+                VStack {
+                    Text("Auston Mattews")
+                        .font(Font.custom("Arial-ItalicMT", size: 25))
+                    Text("Centerman / Toronto Maple Leafs")
+                }
             }
-            .padding()
+            .padding(10)
             
+                HStack{
+                    Text("Height")
+                    Text("Weight")
+                    
+                }
+                
+                HStack{
+                    Text("GP")
+                    Text("G")
+                    Text("A")
+                    Text("PTS")
+                }
+
+                .padding(10)
             Text("Player Description")
+                .bold()
+            
             Text("Auston Taylour Matthews (born September 17, 1997) is an American professional ice hockey center and alternate captain for the Toronto Maple Leafs of the National Hockey League (NHL).")
             
         }
+        .padding()
     }
 }
 
