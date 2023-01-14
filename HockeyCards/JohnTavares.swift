343a9c100f4050c9671f091b8108d5ea325393e6
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-14 10:10:04 -0500

    Finished John Tavares hockey card

--- HockeyCards/JohnTavares.swift
+++ HockeyCards/JohnTavares.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct JohnTavares: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HockeyCardStructure(playerPhoto: "John Tavares", playerNumber: "#91", playerName: "John Tavares", positionAndTeam: "Centerman / Toronto Maple Leafs", playerHeight: "6'1", playerWeight: "216 lb", gamesPlayed: "992", goals: "411", assissts: "527", points: "938", playerDescription: "John Tavares (born September 20, 1990) is a Canadian professional ice hockey centre and captain of the Toronto Maple Leafs of the National Hockey League (NHL). He was selected first overall by the New York Islanders in the 2009 NHL Entry Draft.")
     }
 }
 
---
+++ HockeyCards/WilliamNylander.swift
@@ -0,0 +1,20 @@
+//
+//  WilliamNylander.swift
+//  HockeyCards
+//
+//  Created by Graeme Armstrong on 2023-01-14.
+//
+
+import SwiftUI
+
+struct WilliamNylander: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WilliamNylander_Previews: PreviewProvider {
+    static var previews: some View {
+        WilliamNylander()
+    }
+}
