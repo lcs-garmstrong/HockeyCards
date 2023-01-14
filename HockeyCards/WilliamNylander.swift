8ef55119d5ac36d85c56b6e4f27714982b074fa5
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-14 10:15:08 -0500

    Completed William Nylander Hockey card

--- HockeyCards/WilliamNylander.swift
+++ HockeyCards/WilliamNylander.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct WilliamNylander: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HockeyCardStructure(playerPhoto: "William Nylander", playerNumber: "#88", playerName: "William Nylander", positionAndTeam: "Right Wing / Toronto Maple Leafs", playerHeight: "6'0", playerWeight: "204 lb", gamesPlayed: "482", goals: "159", assissts: "231", points: "390", playerDescription: "William Andrew Michael Junior Nylander Altelius (born 1 May 1996) is a Canadian-born Swedish professional ice hockey right winger for the Toronto Maple Leafs of the National Hockey League (NHL).")
     }
 }
 
