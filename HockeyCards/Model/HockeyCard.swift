0603db67c97d33b6780e1555a9e79b98771880c7
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-16 08:46:29 -0500

    Defined new structure to reduce how much I am repeating myself.

---
+++ HockeyCards/Model/HockeyCard.swift
@@ -0,0 +1,22 @@
+//
+//  HockeyCard.swift
+//  HockeyCards
+//
+//  Created by Graeme Armstrong on 2023-01-16.
+//
+
+import Foundation
+
+struct PlayingCard {
+    let playerPhoto: String
+    let playerNumber: String
+    let playerName: String
+    let positionAndTeam: String
+    let playerHeight: String
+    let playerWeight: String
+    let gamesPlayed: String
+    let goals: String
+    let assissts: String
+    let points: String
+    let playerDescription: String
+}
